a4496f3a75923d015309af0236f91c85dff80791
Kerolos Zaki <ker0olos@pm.me>
2023-06-14 19:05:57 +0300

    Hints (#18)
    * fix #16
    
    * add hints

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,21 +1,29 @@
+import IconMarkdown from 'icons/markdown.tsx';
+
 import type { JSX } from 'preact';
 
 import type { Modify } from '../utils/types.ts';
 
 export default (
-  { label, multiline, value, onInput, ...props }: Modify<
+  { label, hint, multiline, markdown, value, onInput, ...props }: Modify<
     JSX.HTMLAttributes<HTMLInputElement>,
     {
       label?: string;
       hint?: string;
       multiline?: boolean;
+      markdown?: boolean;
       onInput?: (value: string) => void;
     }
   >,
 ) => {
   return (
     <div class={'text-input'}>
-      {label ? <label>{label}</label> : undefined}
+      <div class={'label'}>
+        {label ? <label>{label}</label> : undefined}
+        {markdown ? <IconMarkdown /> : undefined}
+      </div>
+      {hint ? <label class={'hint'}>{hint}</label> : undefined}
+
       {multiline
         ? (
           <textarea
@@ -37,7 +45,6 @@ export default (
             }}
           />
         )}
-      {/* {hint ? <label class={'hint'}>{hint}</label> : undefined} */}
     </div>
   );
 };
