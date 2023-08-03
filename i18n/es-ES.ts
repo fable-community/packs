f9dc77ceec6eac2a11e98e82577f01203a64d400
Luana <luana.latte.cat@gmail.com>
2023-08-03 18:17:21 -0300

    Create es-ES.ts

---
+++ i18n/es-ES.ts
@@ -0,0 +1,74 @@
+export default {
+  noScript: 'javascript es necesario para utilizar esta app!',
+  packTitle: 'Título del pack',
+  loginWithDiscord: 'Ingresar con Discord',
+  logout: 'Cerrar sesión',
+  publish: 'Publicar',
+  save: 'Guardar',
+  export: 'Exportar',
+  import: 'Importar',
+  search: 'Buscar',
+  tabs: ['Personajes', 'Series', 'Colaboradores', 'Desactivar'],
+  okay: 'Okay',
+  name: 'Nombre',
+  title: 'Título',
+  type: 'Tipo',
+  format: 'Formato',
+  none: 'Ninguno',
+  popularity: 'Popularidad',
+  age: 'Edad',
+  gender: 'Género',
+  packAuthor: 'Autor/a del pack',
+  packDescription: 'Descripción del pack',
+  packServers: 'Instalado en % servidores',
+  packWebhookUrl: 'Discord Webhook URL',
+  packWebhookUrlHint:
+    'Cada vez que el pack se actualice, se enviarán los cambios a este webhook',
+  setPublic: 'Establecer como Público',
+  setPrivate: 'Establecer como Privado',
+  privateNotice:
+    'Solo tú y otros colaboradores podrán instalar este pack',
+  placeholder: {
+    gender: 'Clasificado',
+    age: 'Ancestral',
+    packAuthor: 'The Mario Bros',
+    charDescription:
+      'Una chica joven con pelo rosado y ojos rojos, su objetivo en la vida es convertirse en una reina demonio...',
+    mediaDescription:
+      'Un hombre de 30 años es golpeado por un camión y se reencarna en un árbol...',
+    packDescription: 'Un Pack Que Contiene Varios Champiñones',
+  },
+  popularityHint:
+    'Debería ser el número de seguidores / ventas o cualquier otro número que indique qué tan popular es la serie',
+  aliases: 'Tambien conocido/a como',
+  aliasesHint: 'Los nombres alternativos ayudan a mejorar la búsqueda',
+  relations: 'Relaciones',
+  links: 'Enlaces',
+  linksNotice:
+    'Sólo se permiten enlaces a YouTube, Twitch, Crunchyroll, Tapas, Webtoon, y Amazon.',
+  description: 'Descripción',
+  rating: 'Valoración',
+  role: 'Rol',
+  primaryMedia: 'Serie',
+  primaryMediaNotice: 'El personaje no aparecerá en el gacha',
+  success: {
+    title:
+      'Has publicado un nuevo pack exitosamente. Ahora puedes instalarlo usando:',
+    youNeed: 'Necesitas el permiso ',
+    manageServer: 'Administrar Servidor',
+    permissionToInstall:
+      ' para instalar packs. Si no lo tienes puedes pedirle a alguien que lo tenga que instale el pack por tí.',
+  },
+  basedOnMedia: 'Basado en la serie',
+  basedOnIndividual: 'Individual',
+  userId: 'ID de usuario de Discord',
+  addNew: 'Añadir',
+  addNewCharacter: 'Nuevo Personaje',
+  addNewMedia: 'Nueva Serie',
+  maintainersNotice:
+    'Los colaboradores tienen acceso completo a administrar y editar el pack',
+  conflictsNotice:
+    'En la mayoría de casos querrás desactivar las series que tu pack reemplaza o con las que tiene conflicto',
+  deleteMedia: '¿Estás seguro/a de que quieres borrar esta serie?',
+  deleteCharacter: '¿Estás seguro/a de que quieres borrar este personaje?',
+};
