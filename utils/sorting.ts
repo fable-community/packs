649c0418ffb5d3fbad04b2054bad1c33ecca124d
Kerolos Zaki <ker0olos@pm.me>
2023-11-20 00:16:07 +0200

    impl all sorting and orders

--- utils/sorting.ts
+++ utils/sorting.ts
@@ -1,12 +1,15 @@
+import { getRating } from './rating.ts';
+
 import type {
   CharacterSorting,
   DisaggregatedCharacter,
   DisaggregatedMedia,
+  Media,
   MediaSorting,
   SortingOrder,
 } from './types.ts';
 
-const orderByUpdate = (
+const sortByUpdateTimestamp = (
   a: DisaggregatedCharacter | DisaggregatedMedia,
   b: DisaggregatedCharacter | DisaggregatedMedia,
 ) => {
@@ -24,17 +27,115 @@ const orderByUpdate = (
   }
 };
 
+const sortByName = (a: DisaggregatedCharacter, b: DisaggregatedCharacter) => {
+  const nameA = a.name.english ?? '';
+  const nameB = b.name.english ?? '';
+  return nameA.localeCompare(nameB);
+};
+
+const sortByTitle = (a: DisaggregatedMedia, b: DisaggregatedMedia) => {
+  const titleA = a.title.english ?? '';
+  const titleB = b.title.english ?? '';
+  return titleA.localeCompare(titleB);
+};
+
+const sortByPopularity = (a: DisaggregatedMedia, b: DisaggregatedMedia) => {
+  const popularityA = a.popularity ?? 0;
+  const popularityB = b.popularity ?? 0;
+  return popularityB - popularityA;
+};
+
+const sortByRating = (
+  a: DisaggregatedCharacter,
+  b: DisaggregatedCharacter,
+  media: Media[],
+) => {
+  const aPrimaryMedia = a.media?.[0];
+
+  const aPrimaryMediaRef = aPrimaryMedia
+    ? media.find(({ id }) => aPrimaryMedia.mediaId === id)
+    : undefined;
+
+  const aRating = a.popularity
+    ? getRating({ popularity: a.popularity })
+    : getRating({
+      popularity: aPrimaryMediaRef?.popularity ?? 0,
+      role: aPrimaryMediaRef ? aPrimaryMedia?.role : undefined,
+    });
+
+  const bPrimaryMedia = b.media?.[0];
+
+  const bPrimaryMediaRef = bPrimaryMedia
+    ? media.find(({ id }) => bPrimaryMedia.mediaId === id)
+    : undefined;
+
+  const bRating = b.popularity
+    ? getRating({ popularity: b.popularity })
+    : getRating({
+      popularity: bPrimaryMediaRef?.popularity ?? 0,
+      role: bPrimaryMediaRef ? bPrimaryMedia?.role : undefined,
+    });
+
+  return bRating - aRating;
+};
+
+const sortByMediaTitle = (
+  a: DisaggregatedCharacter,
+  b: DisaggregatedCharacter,
+  media: Media[],
+) => {
+  const aPrimaryMedia = a.media?.[0];
+
+  const aPrimaryMediaRef = aPrimaryMedia
+    ? media.find(({ id }) => aPrimaryMedia.mediaId === id)
+    : undefined;
+
+  const aMediaTitle = aPrimaryMediaRef?.title.english ?? '';
+
+  const bPrimaryMedia = b.media?.[0];
+
+  const bPrimaryMediaRef = bPrimaryMedia
+    ? media.find(({ id }) => bPrimaryMedia.mediaId === id)
+    : undefined;
+
+  const bMediaTitle = bPrimaryMediaRef?.title.english ?? '';
+
+  return aMediaTitle.localeCompare(bMediaTitle);
+};
+
+const sortByMediaRole = (
+  a: DisaggregatedCharacter,
+  b: DisaggregatedCharacter,
+) => {
+  const aPrimaryMedia = a.media?.[0];
+
+  const aMediaRole = aPrimaryMedia?.role ?? '';
+
+  const bPrimaryMedia = b.media?.[0];
+
+  const bMediaRole = bPrimaryMedia?.role ?? '';
+
+  return aMediaRole.localeCompare(bMediaRole);
+};
+
 export const sortCharacters = (
-  list: DisaggregatedCharacter[],
+  characters: DisaggregatedCharacter[],
+  media: Media[],
   sorting: CharacterSorting,
   order: SortingOrder,
 ): DisaggregatedCharacter[] => {
-  const sortedList = list.toSorted((a, b) => {
+  const sortedList = characters.toSorted((a, b) => {
     switch (sorting) {
       case 'updated':
-        return orderByUpdate(a, b);
-      default:
-        throw new Error('Not Implemented');
+        return sortByUpdateTimestamp(a, b);
+      case 'name':
+        return sortByName(a, b);
+      case 'rating':
+        return sortByRating(a, b, media);
+      case 'media':
+        return sortByMediaTitle(a, b, media);
+      case 'role':
+        return sortByMediaRole(a, b);
     }
   });
 
@@ -46,14 +147,18 @@ export const sortCharacters = (
 };
 
 export const sortMedia = (
-  list: DisaggregatedMedia[],
+  media: DisaggregatedMedia[],
   sorting: MediaSorting,
   order: SortingOrder,
 ): DisaggregatedMedia[] => {
-  const sortedList = list.toSorted((a, b) => {
+  const sortedList = media.toSorted((a, b) => {
     switch (sorting) {
       case 'updated':
-        return orderByUpdate(a, b);
+        return sortByUpdateTimestamp(a, b);
+      case 'title':
+        return sortByTitle(a, b);
+      case 'popularity':
+        return sortByPopularity(a, b);
       default:
         throw new Error('Not Implemented');
     }
